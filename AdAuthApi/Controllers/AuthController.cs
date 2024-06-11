de1c08cbb93a2c55fbd2ea295ef96c537a76c5a1
Aibono1225 <linyiyin1225@gmail.com>
2024-06-11 23:26:56 +0800

    get user detail using LDAP

--- AdAuthApi/Controllers/AuthController.cs
+++ AdAuthApi/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.DirectoryServices.Protocols;
 
 namespace AdAuthApi.Controllers
 {
@@ -8,12 +9,42 @@ namespace AdAuthApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly string _ldapServer = "ip?";
+        private readonly int _ldapPort = 389;
+        private readonly string _ldapBaseDn = "abc.xyz";
+
         [HttpGet("windows-auth")]
         [Authorize]
         public IActionResult Get()
         {
             var userName = User.Identity.Name;
-            return Ok(new { Message = "Authenticated", User = userName });
+            var userDetails = GetAdUserDetails(userName);
+            return Ok(new { Message = "Authenticated", User = userDetails });
+        }
+
+        private object GetAdUserDetails(string userName)
+        {
+            var ldapConnection = new LdapConnection(new LdapDirectoryIdentifier(_ldapServer, _ldapPort));
+            ldapConnection.AuthType = AuthType.Negotiate;
+            ldapConnection.Bind();
+
+            var searchFilter = $"(sAMAccountName={userName.Split('\\')[1]})";
+            var searchRequest = new SearchRequest(_ldapBaseDn, searchFilter, SearchScope.Subtree, null);
+            var searchResponse = (SearchResponse)ldapConnection.SendRequest(searchRequest);
+
+            if (searchResponse.Entries.Count == 1)
+            {
+                var entry = searchResponse.Entries[0];
+                var userDetails = new
+                {
+                    DisplayName = entry.Attributes["displayName"]?[0].ToString(),
+                    // Other attributes...
+                };
+
+                return userDetails;
+            }
+
+            return null;
         }
     }
 }
