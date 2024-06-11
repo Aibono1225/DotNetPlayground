8039a3382aa75d7d2642f1780ee5c89fc65bfc7f
Aibono1225 <linyiyin1225@gmail.com>
2024-06-11 23:10:19 +0800

    AdAuth - get windows user name

---
+++ AdAuthApi/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("windows-auth")]
+        [Authorize]
+        public IActionResult Get()
+        {
+            var userName = User.Identity.Name;
+            return Ok(new { Message = "Authenticated", User = userName });
+        }
+    }
+}
